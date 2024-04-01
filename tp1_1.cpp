14c4b01189d1973d94b90f0dbe6d30f8e9d6557e
Isas321 <javier.isas321@gmailcom>
2024-04-01 03:05:14 -0300

    Se aplica salto de linea

--- tp1_1.cpp
+++ tp1_1.cpp
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
 int main(){
-    printf("Hola mundo");
+    //printf("Hola mundo");
+    int variable = 2;
+    int *puntero=&variable;
+
+    printf("\nContenido del puntero %d", *puntero);
+    printf("\nDireccion del puntero %d", &puntero);
+    printf("\nDireccion de memoria de la variable %d", variable);
+    printf("\nDireccion de momoria del puntero %d", &*puntero);
+    printf("\nTamanio de memoria utilizado por la variable %d bits", sizeof(variable));
+    printf("\nTamanio de memoria utilizado por el puntero %d bits", sizeof(*puntero));
+    printf("\n\n\n");
     return 0;
 }
